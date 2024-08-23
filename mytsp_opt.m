0ff28ba6f7c314d8acedcae68efeee40c9a29e8d
Gareth Thomas <gareth@versionbay.com>
2024-08-23 13:54:51 +0200

    feat: step 8 - consider another algorithm and see if tests pass and better performance

---
+++ mytsp_opt.m
@@ -0,0 +1,24 @@
+function [best_path, val] = mytsp_opt(G)
+    n = size(G.Nodes,1);
+    all_combinations = perms(1:n);
+    all_combinations(all_combinations(:,1)<n,:) = [];
+    [N,M] = size(all_combinations);
+    
+    % round trip
+    all_combinations(:,n+1) = all_combinations(:,1);
+
+    % Calculate Best Path
+    best = zeros(N,1);
+    for i = 1:N
+        % Calculate Effort path
+        act_path = all_combinations(i,:);
+        for k = 1:M
+            effort = G.Edges.Weight(G.Edges.EndNodes(:,1)==act_path(k) & G.Edges.EndNodes(:,2)==act_path(k+1));
+            best(i) = best(i) + effort;
+        end
+    end
+
+    % find best path:
+    [val,idx] = min(best);
+    best_path = all_combinations(idx(1),:);
+end
--- test_mytsp.m
+++ test_mytsp.m
@@ -1,9 +1,9 @@
-classdef test_mytsp < matlab.unittest.TestCase
+classdef test_mytsp < matlab.perftest.TestCase
 
     properties (TestParameter)
-        N = {2,3,4,5};
-        exp_path = {[2 1 2],[3 1 2 3], [4 1 3 2 4],[5 4 1 3 2 5]};
-        exp_val = {7, 12, 22, 35}
+        N = {2,3,4,5,7};
+        exp_path = {[2 1 2],[3 1 2 3], [4 1 3 2 4],[5 4 1 3 2 5],[7 6 3 2 4 1 5 7]};
+        exp_val = {7, 12, 22, 35, 77}
     end
     methods (Test, ParameterCombination="sequential")
         % Test methods
@@ -11,10 +11,20 @@ classdef test_mytsp < matlab.unittest.TestCase
         function simpleTest(testCase, N, exp_path, exp_val)
             w = magic(N);
             G = digraph(w);
+            testCase.startMeasuring()
             [best_path,val] = mytsp(G);
+            testCase.stopMeasuring()
+            testCase.verifyEqual(best_path, exp_path);
+            testCase.verifyEqual(val, exp_val);
+        end
+        function simpleNewTest(testCase, N, exp_path, exp_val)
+            w = magic(N);
+            G = digraph(w);
+            testCase.startMeasuring()
+            [best_path,val] = mytsp_opt(G);
+            testCase.stopMeasuring()
             testCase.verifyEqual(best_path, exp_path);
             testCase.verifyEqual(val, exp_val);
         end
     end
-
 end
